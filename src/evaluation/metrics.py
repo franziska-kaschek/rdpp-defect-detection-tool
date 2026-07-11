1b45bf2c38c4ff2cbceb3287e8204edc1418e03f
Franziska Kaschek <franziska@kaschek.net>
2026-07-11 22:52:42 +0200

    Remove print-statements in compute_pro

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -144,9 +144,6 @@ def compute_pro(anomaly_maps, ground_truth_maps):
         fprs: numpy array of false positive rates.
         pros: numpy array of corresponding PRO values.
     """
-
-    print("Compute PRO curve...")
-
     # Structuring element for computing connected components.
     structure = np.ones((3, 3), dtype=int)
 
@@ -200,7 +197,6 @@ def compute_pro(anomaly_maps, ground_truth_maps):
     pro_changes_flat = pro_changes.ravel()
 
     # Sort all anomaly scores.
-    print(f"Sort {len(anomaly_scores_flat)} anomaly scores...")
     sort_idxs = np.argsort(anomaly_scores_flat).astype(np.uint32)[::-1]
 
     # Info: np.take(a, ind, out=a) followed by b=a instead of
